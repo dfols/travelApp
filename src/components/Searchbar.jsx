961d2e4f9062049f90d22ba83c1934cc1adafe09
asmeretghm <asmeretghm@gmail.com>
2024-07-11 19:14:18 -0400

    search bar with input and button

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+
+    return (
+        <>
+            <div>
+                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            </div>
+            <button className="btn">Button</button>
+
+        </>
+    )
+}
+
+export default SearchBar
