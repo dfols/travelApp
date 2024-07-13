22af637a2d619b6e7f56bf7e9ef32b68c527def7
Brandon <btayloragent@gmail.com>
2024-07-12 20:16:19 -0400

    updated result component pt1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Searchbar from "./components/Searchbar"
+import Result from "./Result"
 
 function App() {
   return (
     <>
       <div className="App">
         <Searchbar />
+        <Result name= "Cosi" description= "Science Center" address= " 333 W Broad St" types= "places to go" photoReference= "image"/>
       </div>
 
     </>
