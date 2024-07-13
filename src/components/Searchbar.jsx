74440214ffa7e748fb44d0d1bc38dcfd595f7a3e
asmeretghm <asmeretghm@gmail.com>
2024-07-12 20:29:19 -0400

    onClick and onChange event handler

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,14 +1,39 @@
+import React, { useState } from 'react'
 function SearchBar() {
+
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+        console.log(searchTerm)
+    };
+    const handleSearch = () => {
+        console.log(searchTerm);
+
+    }
+
+
+
 
     return (
         <>
             <div>
-                <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <input type="text" placeholder="Type search term here ..."
+                    className="input input-bordered w-full max-w-xs"
+                    value={searchTerm}
+                    onChange={handleInputChange} />
             </div>
-            <button className="btn">Button</button>
+            <button onClick={handleSearch} className="btn">Search</button>
 
         </>
+
+
     )
 }
 
+
 export default SearchBar
+
+
+
+
